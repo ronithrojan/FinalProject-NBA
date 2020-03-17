e65626130228ad4014ab71f99cc2d61da04f00c5
Grant Flennoy <flennoyg@gmail.com>
2020-03-17 10:43:19 -0400

    added get scores & commented out stuff

--- src/app/triviaquiz.service.ts
+++ src/app/triviaquiz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { format } from "path";
 
 @Injectable({
   providedIn: "root"
@@ -10,10 +11,26 @@ export class TriviaquizService {
     username: "",
     score: 0
   };
+  // questionsAndAnswers: QAndA = { questions: [], answers: {} };
 
   constructor(private http: HttpClient) {}
 
   getQuestions() {
     return this.http.get(`${this.baseUrl}/questions`);
   }
+  getScore() {
+    return this.http.get(`${this.baseUrl}/scores`);
+  }
+  // calculateScore() {
+  //   this.lastPlayer.username = form.value.username;
+  //   this.lastPlayer.score = 0;
+  //   this.questionsAndAnswers.questions = questions;
+  //   this.questionsAndAnswers.answers = form.value;
+  //   this.questionsAndAnswers.forEach((question, index) => {
+  //     if (question.answer === format.value[index]) {
+  //       this.lastPlayer.score++;
+  //     }
+  //   });
+  //   this.addScore(this.lastPlayer).subscribe();
+  // }
 }
