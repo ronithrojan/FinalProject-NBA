112b20dd44356d338fba695dd1c5853db8cca43d
Melissa Rice <mmrice22@gmail.com>
2020-03-18 14:36:09 -0400

    added header and routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from "@angular/router";
 import { QuizComponent } from "./quiz/quiz.component";
 import { ScoreComponent } from "./score/score.component";
 import { GooglemapComponent } from "./googlemap/googlemap.component";
+import { HeaderComponent } from "./header/header.component";
 
 const routes: Routes = [
   { path: "", redirectTo: "/quiz", pathMatch: "full" },
   { path: "quiz", component: QuizComponent },
   { path: "scores", component: ScoreComponent },
   { path: "googlemap", component: GooglemapComponent },
+  { path: "header", component: HeaderComponent },
   { path: "**", component: QuizComponent }
 ];
 
